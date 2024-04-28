7c34869c2c2d8312289c3ec1e76ceb07db9b03bd
VincentCorgi <a325698741z@gmail.com>
2024-04-28 16:29:28 +0800

    config: add eslint

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
-import http from 'http'
-import app from './src/app'
+import http from "http";
+import app from "./src/app";
 
-const server = http.createServer(app)
-const PORT = process.env.PORT || 3000
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-  console.log(`http://localhost:${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
