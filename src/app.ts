3e9bb1ebe735358967f52c4e433fbde01c2ba873
VincentCorgi <a325698741z@gmail.com>
2024-04-28 16:27:14 +0800

    fix: format tab two

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req: Request, res: Response) => {
-    res.send("hello corgi");
+  res.send("hello corgi");
 });
 
 export default app;
