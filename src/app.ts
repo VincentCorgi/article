191caf8ebb7f93ebacb462dcf61907ad87e9dd4e
VincentCorgi <a325698741z@gmail.com>
2024-04-28 21:01:18 +0800

    fix: add log request

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 const app: Express = express();
+const logger =
+    (req: Request, res: Response, next: NextFunction) => {
+      const timestamp = new Date().toLocaleTimeString();
+      const {method, originalUrl} = req;
+      console.log(`${timestamp} ${method} ${originalUrl} `);
+      if (Object.keys(req.body).length !== 0) console.log(`Body: ${JSON.stringify(req.body)}`);
+      if (Object.keys(req.params).length !== 0) console.log(`Params: ${JSON.stringify(req.params)}`);
+      if (Object.keys(req.query).length !== 0) console.log(`Query: ${JSON.stringify(req.query)}`);
+      next();
+    };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(logger);
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("hello corgi");
