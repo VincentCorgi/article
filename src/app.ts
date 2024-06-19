d14353add8f2f7d0161944bc2d8ad333da7f5ab0
VincentCorgi <a325698741z@gmail.com>
2024-06-19 15:47:51 +0800

    comment no use code

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
 import express, { Express, Request, Response } from "express";
-import routes from "./routes";
-import errorHandler from "./util/middleware/errorHandler";
-import logger from "./util/middleware/logger";
+// import routes from "./routes";
+// import errorHandler from "./util/middleware/errorHandler";
+// import logger from "./util/middleware/logger";
 const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger);
+// app.use(logger);
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("hello world");
 });
 
 // routes
-routes(app);
+// routes(app);
 
 // error handler
-app.use(errorHandler);
+// app.use(errorHandler);
 
 export default app;
